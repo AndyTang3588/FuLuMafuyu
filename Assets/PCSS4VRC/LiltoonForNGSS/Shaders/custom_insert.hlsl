fff1be339cfa09111183097fbb45b94fb904a023
FuLu糖福禄 <tanghaochen0506@hotmail.com>
2025-09-13 21:02:12 +0200

    头部骨骼移植（bug，头部靠近消失）

--- Assets/PCSS4VRC/LiltoonForNGSS/Shaders/custom_insert.hlsl
+++
@@ -1,5 +0,0 @@
-#undef unity_LightShadowBias
-
-#define BEFORE_UNPACK_V2F \
-	NGSS_GLOBAL_OPACITY = (NGSS_GLOBAL_OPACITY * (1 - _UseMaskTex)) + ((1 - LIL_SAMPLE_2D(_MaskTex,sampler_MaskTex, fd.uv0).r)* _UseMaskTex);
-
--- Assets/PCSS4VRC/LiltoonForSimplePCSS/Shaders/custom_insert.hlsl
+++
@@ -1,4 +0,0 @@
-#undef unity_LightShadowBias
-
-//#define BEFORE_UNPACK_V2F \
-	_EnvLightStrength = (_EnvLightStrength * (1 - _UseMaskTex)) + ((1 - LIL_SAMPLE_2D(_MaskTex,sampler_MaskTex, fd.uv0).r)* _UseMaskTex);
